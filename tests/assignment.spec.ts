44f6b24839b85e50b5c48958a886beb20281f622
Aman082000 <aman.bhardwaj0820@gmail.com>
2026-07-26 13:35:29 +0530

    RefactoringCode

--- tests/assignment.spec.ts
+++ tests/assignment.spec.ts
@@ -3,41 +3,38 @@ import fs from 'fs';
 
 
 test('Navigate to books store application', async ({ page }) => {
+  const USERNAME = 'saksam0808';
+  const PASSWORD = 'S@ksam2025';
+  const BOOK_NAME = 'Learning JavaScript Design Patterns';
+
   await page.goto('https://demoqa.com/');
 
   //Navigate to Books Store Application
-  await page.locator('a[href="/books"]')
-            .locator('.card')
-            .locator('.card-body')
-            .filter({hasText: "Book Store Application"})
-            .click();
+  await page.locator('.card').filter({hasText: "Book Store Application"}).click();
 
   //Login using the newly created user.
   //Username - saksam0808, Password - S@ksam2025
   await page.getByRole('button', {name: "Login"}).click();
 
-  const username = 'saksam0808';
-  const password = 'S@ksam2025';
-  await page.getByPlaceholder('UserName').fill(username);
-  await page.getByPlaceholder('Password').fill(password);
+  await page.getByPlaceholder('UserName').fill(USERNAME);
+  await page.getByPlaceholder('Password').fill(PASSWORD);
   await page.locator('#login').click();
 
   // Upon successful login, Validate username and logout button.
-  const usernameVisible = await page.locator('#userName-value').textContent();
-  expect(usernameVisible).toEqual(username);
-
-  const logoutButtonVisible = await page.getByRole('button', {name: "Logout"}).isVisible();
-  expect(logoutButtonVisible).toBeTruthy()
+  await expect(page.locator('#userName-value')).toHaveText(USERNAME);
+  await expect(page.getByRole('button', {name: "Logout"})).toBeVisible();
 
   //Click on bookstore button
-  await page.locator('#item-2 a[href="/books"]').filter({hasText: "Book Store"}).click({ force: true });
+  await page.locator('#item-2 a[href="/books"]').filter({hasText: "Book Store"})
+            .click({ force: true });
 
   // Search "Learning JavaScript Design Patterns"
-  const bookToSearch = 'Learning JavaScript Design Patterns';
-  await page.getByPlaceholder('Type to search').fill(bookToSearch);
+  const searchBox = await page.getByPlaceholder('Type to search');
 
-  const filledInputText = await page.getByPlaceholder('Type to search').inputValue();
-  expect(filledInputText).toEqual(bookToSearch);
+  await searchBox.fill(BOOK_NAME);
+  
+  const filledInputText = await searchBox.inputValue();
+  expect(filledInputText).toEqual(BOOK_NAME);
 
   // Validate the search result to contain this book.
   const searchResultVisible = await page.getByRole('link',{name: 'Learning JavaScript Design Patterns'}).isVisible();
@@ -46,9 +43,11 @@ test('Navigate to books store application', async ({ page }) => {
   await expect(page.getByText("O'Reilly Media")).toBeVisible();
 
   // Print Title, Author and Publisher into a file.
-  const title = await page.locator('tr').nth(1).locator('td').nth(1).innerText();
-  const author = await page.locator('tr').nth(1).locator('td').nth(2).innerText();
-  const publisher = await page.locator('tr').nth(1).locator('td').nth(3).innerText();
+  const row = page.locator('tr').nth(1).locator('td');
+
+  const title = await row.nth(1).innerText();
+  const author = await row.nth(2).innerText();
+  const publisher = await row.nth(3).innerText();
   console.log(`${title} ${author} ${publisher}`);
 
   const data = `Title: ${title}
@@ -56,10 +55,10 @@ test('Navigate to books store application', async ({ page }) => {
   Publisher: ${publisher}`
 
   fs.writeFileSync('searchResult.txt', data);
-  console.log('Data writtent to searchResult.txt');
+  console.log('Data written to searchResult.txt');
   
   // Click on log out
-
+  await page.getByRole('button', {name: "Log out"}).click();
 
 });
 
